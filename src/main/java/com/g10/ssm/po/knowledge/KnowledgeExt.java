9d1887914014110424ad6c77b4c941546bdba52a
Lenovo-PC\asua <597376452@qq.com>
2017-08-22 15:43:30 +0800

    no message

---
+++ src/main/java/com/g10/ssm/po/knowledge/KnowledgeExt.java
@@ -0,0 +1,15 @@
+package com.g10.ssm.po.knowledge;
+
+import com.g10.ssm.po.knowledge.Knowledge;
+
+public class KnowledgeExt extends Knowledge {
+	String categoryName;
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
